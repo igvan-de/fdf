8a8ed822b6ea467a032c7a7283c00dcb4a348084
Igor borys oscar Van der smissen <igvan-de@f0r3s17.codam.nl>
2019-05-10 19:05:50 +0200

    10-05-2019 19:05

--- read.c
+++ read.c
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   read.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: igvan-de <igvan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/10 11:55:46 by igvan-de       #+#    #+#                */
-/*   Updated: 2019/05/10 13:23:10 by igvan-de      ########   odam.nl         */
+/*   Updated: 2019/05/10 19:05:27 by igvan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <fcntl.h>
 
-int     main(int ag, char **av)
+int		main(int ag, char **av)
 {
-    char    *line;
-    int     ret;
-    int     fd;
+	char	*line;
+	int		ret;
+	int		fd;
 
-    ret = 1;
-    fd = open(av[1], O_RDONLY);
-    if (ag != 2)
-        return (FALSE);
-    while (ret > 0)
-    {
-        ret = get_next_line(fd, &line);
-        free(line);
-        if (ret == -1)
-            break;
-    }
-    return (0);
+	ret = 1;
+	fd = open(av[1], O_RDONLY);
+	if (ag != 2)
+		return (FALSE);
+	while (ret > 0)
+	{
+		ret = get_next_line(fd, &line);
+		free(line);
+		if (ret == -1)
+			break ;
+	}
+	return (0);
 }
